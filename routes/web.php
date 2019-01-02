63bc1915d763da5a56a4faf28b32ec3d59ffe58f
3A532055 <ylhc214044@gmail.com>
2019-01-02 12:01:43 +0800

    新增暫定路徑

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/account', 'HomeController@account')->name('account');
+Route::get('/comment', 'HomeController@comment')->name('comment');
+
